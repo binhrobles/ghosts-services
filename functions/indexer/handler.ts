7566d9ae2cdc0635d63b3a6e8c7ba942e44e6b3f
Binh 🌱 <binhrobles@gmail.com>
2020-06-26 00:05:23 -0700

    indexer: scaffold stream event types

--- functions/indexer/handler.ts
+++ functions/indexer/handler.ts
@@ -1,11 +1,38 @@
-import { DynamoDBStreamHandler } from 'aws-lambda';
+import { DynamoDBStreamHandler, DynamoDBStreamEvent } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const IndexRecords: DynamoDBStreamHandler = (event) => {
-  console.log(JSON.stringify(event, null, 2));
-  event.Records.forEach(function (record) {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
+export const IndexRecords: DynamoDBStreamHandler = async (
+  event: DynamoDBStreamEvent
+) => {
+  event.Records.forEach((record) => {
+    console.log(JSON.stringify(record.dynamodb, null, 2));
+    switch (record.eventName) {
+      case 'INSERT':
+        // check first, then post to es index
+        // for idempotency
+        console.log('INSERT triggered');
+        break;
+      case 'MODIFY':
+        // post to es index
+        console.log('MODIFY triggered');
+        break;
+      case 'REMOVE':
+        // delete from es index
+        console.log('REMOVE triggered');
+        break;
+      default:
+        console.log(
+          JSON.stringify(
+            {
+              _id: record.dynamodb.Keys['_id'].S,
+              streamEvent: record.eventName,
+              streamID: record.eventID,
+              message: 'Ignoring stream event',
+            },
+            null,
+            2
+          )
+        );
+    }
   });
 };
