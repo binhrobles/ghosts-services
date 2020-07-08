46bca4593875b23f462b997f31979d8e7fd27738
Binh 🌱 <binhrobles@gmail.com>
2020-07-08 16:57:14 -0700

    Allow embedded image links

--- src/entries/lib/sanitize.test.ts
+++ src/entries/lib/sanitize.test.ts
@@ -2,13 +2,14 @@
 
 import { sanitizeText } from './sanitize';
 
-test('cleans html of img and random-tag', () => {
+test('cleans html of bad img and random-tag', () => {
   const funkyHtml =
-    "<random-tag><h3>What's worth fighting for?</h3><p><br></p><ul><li>echoes</li></ul><p><br></p><p>         foobar</p><p><br></p><p><br></p><p>fdasjkl;</p><img>hey</img><div>null</div></random-tag>";
+    "<random-tag><h3>What's worth fighting for?</h3><p><br></p><ul><li>echoes</li></ul><p><br></p><p>         foobar</p><p><br></p><p><br></p><p>fdasjkl;</p><img>hey</img><img src='https://i.imgur.com/wPwD6xM.jpg'><div>null</div></random-tag>";
 
   const response = sanitizeText(funkyHtml);
 
   expect(response).toBeDefined();
   expect(response.includes('<random-tag>')).toBeFalsy();
   expect(response.includes('<img>')).toBeFalsy();
+  expect(response.includes('<img src=')).toBeTruthy();
 });
--- src/entries/lib/sanitize.ts
+++ src/entries/lib/sanitize.ts
@@ -2,5 +2,7 @@ import sanitizeHtml from 'sanitize-html';
 
 export const sanitizeText = (text: string): string => {
   // clean up user html input
-  return sanitizeHtml(text);
+  return sanitizeHtml(text, {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['h1', 'h2', 'img']),
+  });
 };
