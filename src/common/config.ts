8b2366d831dc642809931cafcf81fa1427ea1754
Binh 🌱 <binhrobles@gmail.com>
2020-07-07 18:39:37 -0700

    Update CORS allowed origins for new site

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,6 +1,6 @@
 const config = Object.freeze({
   allowedOrigins:
-    process.env.STAGE === 'production' ? 'https://binhrobles.com' : '*',
+    process.env.STAGE === 'production' ? 'https://ghosts.binhrobles.com' : '*',
 });
 
 export default config;
