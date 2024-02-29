a49d08980d5be084ff92aafeb324406d356d5432
Binh <binhrobles@gmail.com>
2024-02-29 13:24:13 -0800

    add ddb stream

--- infrastructure/modules/backend/dynamo.tf
+++ infrastructure/modules/backend/dynamo.tf
@@ -6,6 +6,9 @@ resource "aws_dynamodb_table" "entries_table" {
   read_capacity  = 1
   write_capacity = 1
 
+  stream_enabled = true
+  stream_view_type = "NEW_IMAGE"
+
   /* uses a service-generated ID for the partition key */
   hash_key  = "id"
 
